b2a99d20e46983f8a999cabcd5bc5eda2acb6e36
jan <xxmaxmustermannxx@gmx.de>
2021-07-09 14:56:00 +0200

    css

--- static/popup.js
+++ static/popup.js
@@ -1,7 +1,7 @@
 function show_popup(id) {
     // Get DOM object
     let popup = document.getElementById('id');
-    if (!popup || popup.style.getPropertyValue('visibility')==='visible') return;
+    if (!popup || popup.style.getPropertyValue('display')==='block') return;
 
     // Hide others
     for (const other in document.getElementsByClassName('popup')) hide_popup(other.id);
@@ -13,17 +13,17 @@ function show_popup(id) {
     document.getElementsByClassName('body')[0].appendChild(bg);
 
     // Show popup
-    popup.style.setProperty('visibility', 'visible');
+    popup.style.setProperty('display', 'block');
 }
 
 function hide_popup(id) {
     // Get DOM object
     let popup = document.getElementById('id');
-    if (!popup || popup.style.getPropertyValue('visibility')==='hidden') return;
+    if (!popup || popup.style.getPropertyValue('display')==='none') return;
 
     // Remove background
     for (const bg in document.getElementsByClassName('popup_background')) document.removeChild(bg);
 
     // Hide popup
-    popup.style.setProperty('visibility', 'hidden');
+    popup.style.setProperty('display', 'none');
 }
