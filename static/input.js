9ed6151b1c4708dae406b1d7e28bf4bd030eae5f
jan <xxmaxmustermannxx@gmx.de>
2021-09-06 17:17:44 +0200

    input box

---
+++ static/input.js
@@ -0,0 +1,16 @@
+// Restricts input for the given textbox to 0-9; A-Z
+function setInputFilter(textbox) {
+    for (const event of ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"]) {
+        textbox.addEventListener(event, () => {
+            let out = ""
+            for (const char of textbox.value) {
+                let upper = char.toUpperCase()
+                if (('0' <= upper && '9' <= upper) || ('A' <= upper && 'Z' <= upper)) out += upper
+            }
+            textbox.value = out
+        })
+    }
+}
+
+let keyBox = document.getElementById("new_user_key")
+if (keyBox) setInputFilter(keyBox)
