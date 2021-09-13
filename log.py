b7fa49f7fa0462fa0b36f24ddb4c8c70a1c128da
jan <xxmaxmustermannxx@gmx.de>
2021-09-13 18:16:14 +0200

    quickpush

--- log.py
+++ log.py
@@ -2,8 +2,8 @@ from logging import getLogger
 
 
 def warn(*args):
-    getLogger().warning(' '.join(args))
+    getLogger().warning(' '.join([str(arg) for arg in args]))
 
 
 def info(*args):
-    getLogger().info(' '.join(args))
+    getLogger().info(' '.join([str(arg) for arg in args]))
