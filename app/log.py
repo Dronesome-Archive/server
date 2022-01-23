fed1f57eb1aadf04e0c3c2884b1fd39052ed07dd
CheeseCrustery <xxmaxmustermannxx@gmx.de>
2022-01-23 09:20:31 +0100

    logging config

--- app/log.py
+++ app/log.py
@@ -5,16 +5,21 @@ l = None
 def init():
     config.dictConfig({
         'version': 1,
-        'formatters': {'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-        }},
-        'handlers': {'wsgi': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://flask.logging.wsgi_errors_stream',
-            'formatter': 'default'
-        }},
+        'formatters': {
+            'default': {
+                'format': '[%(asctime)s][%(levelname)s][%(module)s]:\t%(message)s',
+                'datefmt': '%H:%M:%S',
+            }
+        },
+        'handlers': {
+            'wsgi': {
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://flask.logging.wsgi_errors_stream',
+                'formatter': 'default',
+            }
+        },
         'root': {
             'level': 'INFO',
-            'handlers': ['wsgi']
+            'handlers': ['wsgi'],
         }
     })
