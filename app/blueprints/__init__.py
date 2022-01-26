3ccefa618db27152f2ecea0a8a91bc7644dfc1ed
CheeseCrustery <xxmaxmustermannxx@gmx.de>
2022-01-26 15:23:55 +0100

    forgot to register drone_api blueprint

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -1,6 +1,7 @@
 from app.blueprints.auth import auth
 from app.blueprints.drone_control import drone_control
+from app.blueprints.drone_api import drone_api
 from app.blueprints.pages import pages
 from app.blueprints.users import users
 
-blueprints = [auth, drone_control, pages, users]
+blueprints = [auth, drone_control, drone_api, pages, users]
