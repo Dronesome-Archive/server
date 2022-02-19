99f851244280c2d0333293eb728df0c31ea46ada
CheeseCrustery <xxmaxmustermannxx@gmx.de>
2022-02-19 19:37:19 +0100

    fixed indentation

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -17,56 +17,56 @@ auth = flask.Blueprint('auth', __name__, url_prefix='/auth')
 # Create user object from db for flask_login
 @login.user_loader
 def load_user(login_id):
-    user = db.users.find_one({'login_id': ObjectId(login_id)})
-    if not user:
-        return None
-    else:
-        return User(user)
+	user = db.users.find_one({'login_id': ObjectId(login_id)})
+	if not user:
+		return None
+	else:
+		return User(user)
 
 
 # Redirect user to the OAuth provider
 @auth.route('/<string:oauth_server>/login')
 def login(oauth_server):
-    if client := oauth.create_client(oauth_server):
-        callback_url = flask.url_for('.callback', oauth_server=oauth_server, _external=True, _scheme='https')
-        return client.authorize_redirect(callback_url)
-    logging.warning(f'Invalid oauth server: {oauth_server}')
-    flask.flash('Ungültige Anmeldemethode!', 'error')
-    return redirect(flask.url_for('pages.sign_in'))
+	if client := oauth.create_client(oauth_server):
+		callback_url = flask.url_for('.callback', oauth_server=oauth_server, _external=True, _scheme='https')
+		return client.authorize_redirect(callback_url)
+	logging.warning(f'Invalid oauth server: {oauth_server}')
+	flask.flash('Ungültige Anmeldemethode!', 'error')
+	return redirect(flask.url_for('pages.sign_in'))
 
 
 # With the received OAuth credentials, redirect either to the home page or the account creation page
 @auth.route('/<string:oauth_server>/callback')
 def callback(oauth_server):
-    if client := oauth.create_client(oauth_server):
+	if client := oauth.create_client(oauth_server):
 
-        # Use authorization code to fetch OIDC info (https://openid.net/specs/openid-connect-core-1_0.html#IDToken)
-        id_token = client.authorize_access_token()
-        userinfo = client.parse_id_token(id_token)
-        if user := db.users.find_one({'oauth.server': oauth_server, 'oauth.token': userinfo.sub}):
+		# Use authorization code to fetch OIDC info (https://openid.net/specs/openid-connect-core-1_0.html#IDToken)
+		id_token = client.authorize_access_token()
+		userinfo = client.parse_id_token(id_token)
+		if user := db.users.find_one({'oauth.server': oauth_server, 'oauth.token': userinfo.sub}):
 
-            # Account already exists
-            flask_login.login_user(User(user))
-            return redirect(flask.url_for('pages.account'))
-        else:
+			# Account already exists
+			flask_login.login_user(User(user))
+			return redirect(flask.url_for('pages.account'))
+		else:
 
-            # New account
-            flask.session['oauth_server'] = oauth_server
-            flask.session['oauth_token'] = userinfo.sub
-            return redirect(flask.url_for('pages.register'))
-    else:
-        logging.warning(f'Invalid oauth server: {oauth_server}')
-        flask.flash('Ungültige Anmeldemethode!', 'error')
-        return redirect(flask.url_for('pages.sign_in'))
+			# New account
+			flask.session['oauth_server'] = oauth_server
+			flask.session['oauth_token'] = userinfo.sub
+			return redirect(flask.url_for('pages.register'))
+	else:
+		logging.warning(f'Invalid oauth server: {oauth_server}')
+		flask.flash('Ungültige Anmeldemethode!', 'error')
+		return redirect(flask.url_for('pages.sign_in'))
 
 
 # Invalidate the session and bring back to login page
 @auth.route('/logout')
 @flask_login.login_required
 def logout():
-    db.users.update_one({'_id': ObjectId(flask_login.current_user.id_str)}, {'$set': {'login_id': ObjectId()}})
-    flask_login.logout_user()
-    flask.flash('Ausgeloggt.')
+	db.users.update_one({'_id': ObjectId(flask_login.current_user.id_str)}, {'$set': {'login_id': ObjectId()}})
+	flask_login.logout_user()
+	flask.flash('Ausgeloggt.')
 	if (flask_login.current_user.get()['oauth']['server'] == 'google'):
 		# https://stackoverflow.com/q/4202161/
 		return redirect('https://accounts.google.com/logout?continue=https://appengine.google.com/_ah/logout?continue=https://droneso.me/')
