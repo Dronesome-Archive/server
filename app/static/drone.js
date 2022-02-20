a9a2832334a4d18d2df2aeb6676fe860b749a18f
CheeseCrustery <xxmaxmustermannxx@gmx.de>
2022-02-20 11:32:46 +0100

    reworked button display system

--- app/static/drone.js
+++ app/static/drone.js
@@ -8,6 +8,7 @@ let batteryDisplay;
 let stateDisplay;
 let stateContainer;
 
+// API
 let accessToken = '';
 let canControl = false;
 
@@ -29,39 +30,48 @@ let currentFacilityLine = null;
 
 
 function init() {
-    // get DOM elements
-    droneButtons = document.getElementById('drone_buttons');
-    batteryDisplay = document.getElementById('battery_display');
-    stateDisplay = document.getElementById('state_display');
-    stateContainer = document.getElementById('state_container');
+	// get DOM elements
+	batteryDisplay = document.getElementById('battery_display');
+	stateDisplay = document.getElementById('state_display');
+	stateContainer = document.getElementById('state_container');
 
-    // init map
-    let map = L.map('map').setView(ownFacility.pos, 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: accessToken
-    }).addTo(map);
-    droneMarker = L.marker([0.0, 0.0], {title: 'Kurier'});
-    for (let facilityId in facilities) {
-        facilityMarkers[facilityId] = L.marker(facilities[facilityId].pos, {title: facilities[facilityId].name}).addTo(map);
-        if (facilities[facilityId] !== homeFacility) {
-            facilityLines[facilityId] = L.polyline(
-                [facilities[facilityId].pos].concat(facilities[facilityId].waypoints).concat([homeFacility.pos]),
-                {color: inactiveLineCol}
-            ).addTo(map);
-        }
-    }
+	// create drone buttons
+	for (const button in ['request', 'allowTakeoff', 'emergencyReturn', 'emergencyLand', 'crashed']) {
+		droneButtons[button] = createButton(document.getElementById('drone_buttons'));
+	}
+	droneButtons.request.show = showRequest;
+	droneButtons.allowTakeoff.show = showAllowTakeoff;
+	droneButtons.emergencyReturn.show = showEmergencyReturn;
+	droneButtons.emergencyLand.show = showEmergencyLand;
+	droneButtons.crashed.show = showCrashed;
 
-    // connect via socketio
-    const socket = io('/frontend');
-    socket.on('facility_state', onFacilityState);
-    socket.on('drone_state', onDroneState);
-    socket.on('heartbeat', onHeartbeat);
-    socket.on('drone_requested', onDroneRequested);
+	// init map
+	let map = L.map('map').setView(ownFacility.pos, 13);
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken: accessToken
+	}).addTo(map);
+	droneMarker = L.marker([0.0, 0.0], {title: 'Kurier'});
+	for (let facilityId in facilities) {
+		facilityMarkers[facilityId] = L.marker(facilities[facilityId].pos, {title: facilities[facilityId].name}).addTo(map);
+		if (facilities[facilityId] !== homeFacility) {
+			facilityLines[facilityId] = L.polyline(
+				[facilities[facilityId].pos].concat(facilities[facilityId].waypoints).concat([homeFacility.pos]),
+				{color: inactiveLineCol}
+			).addTo(map);
+		}
+	}
+
+	// connect via socketio
+	const socket = io('/frontend');
+	socket.on('facility_state', onFacilityState);
+	socket.on('drone_state', onDroneState);
+	socket.on('heartbeat', onHeartbeat);
+	socket.on('drone_requested', onDroneRequested);
 }
 
 
@@ -69,64 +79,57 @@ function init() {
 // BUTTONS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function showButton(text, classList) {
-    let button = document.createElement('button');
-    button.classList.add('item');
-    for (const c of classList) {
-        button.classList.add(c);
-    }
-    button.innerText = text;
-    droneButtons.appendChild(button);
-    return button;
+function createButton(parent) {
+	let button = document.createElement('button');
+	button.style.display = 'none';
+	parent.appendChild(button);
+	return {
+		dom: button,
+		show: null,
+		hide: function() {
+			this.dom.className = 'item';
+			this.dom.style.display = 'none';
+			this.dom.onclick = null;
+			this.dom.innerText = '';
+		}
+	}
 }
 
-function showTakeoff() {
-    let button = showButton('Starterlaubnis erteilen', ['good_step']);
-    button.onclick = () => {
-        window.location.href = '/drone_control/allow_takeoff';
-    };
+showRequest = function(requested) {
+	this.button.style.display = 'initial';
+	if (requested) {
+		this.button.innerText = "Kurier angefordert";
+	} else {
+		this.button.innerText = "Kurier anfordern";
+		this.button.classList.add('good_step');
+		this.button.onclick = () => window.location.href = '/drone_control/request';
+	}
 }
 
-function showReturn() {
-    let button = showButton('Sofort umkehren', ['bad_step']);
-    button.onclick = () => {
-        show_popup('emergency_return');
-    };
+showAllowTakeoff = function() {
+	this.button.style.display = 'initial';
+	this.button.innerText = "Starterlaubnis erteilen";
+	this.button.classList.add('good_step');
 }
 
-function showLand() {
-    let button = showButton('Notlanden', ['bad_step']);
-    button.onclick = () => {
-        show_popup('emergency_land');
-    };
+showEmergencyReturn = function() {
+	this.button.style.display = 'initial';
+	this.button.innerText = "Sofort umkehren";
+	this.button.classList.add('bad_step');
+	this.button.onclick = () => showPopup('emergency_return');
 }
 
-function showRequest() {
-    if (ownFacility == homeFacility) return;
-    let canRequest = (ownFacility != goalFacility) && canControl && !droneRequested;
-    let button = document.getElementById('request');
-    if (!button) {
-        button = showButton(
-            canRequest ? "Kurier anfordern" : "Kurier angefordert",
-            canRequest ? ['good_step'] : []
-        );
-    }
-    if (canRequest) {
-        button.onclick = () => {
-            window.location.href = '/drone_control/request';
-        }
-    } else {
-        button.onclick = null;
-    }    
-    button.id = 'request';
+showEmergencyLand = function() {
+	this.button.style.display = 'initial';
+	this.button.innerText = "Sofort notlanden";
+	this.button.classList.add('bad_step');
+	this.button.onclick = () => showPopup('emergency_land');
 }
 
-function showEmergency() {
-    let button = showButton(
-        "Der Kurier ist notgelandet und muss sofort von einem Mitarbeiter geborgen werden",
-        ['bad_step']
-    )
-    button.disabled = true;
+showCrashed = function(show) {
+	this.button.style.display = 'initial';
+	this.button.innerText = "Notgelandet";
+	this.button.classList.add('bad_step');
 }
 
 
@@ -135,68 +138,67 @@ function showEmergency() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function onFacilityState(args) {
-    console.log('onFacilityState', args);
+	console.log('onFacilityState', args);
+	goalFacility = facilities[args.goal_id];
 
-    // show request button
-    goalFacility = facilities[args.goal_id];
-    showRequest();
+	// highlight goal
+	facilityMarkers[goalFacility.id].color = '#FF0000';
 
-    // highlight goal
-    facilityMarkers[goalFacility.id].color = '#FF0000';
+	// show / hide drone state
+	if (args.state === 'idle' && goalFacility != ownFacility) {
+		stateContainer.style.display = 'none';
+	} else {
+		stateContainer.style.display = 'initial';
+	}
 
-    // show / hide drone state
-    if (args.state === 'idle' && goalFacility != ownFacility) {
-        stateContainer.style.display = 'none';
-    } else {
-        stateContainer.style.display = 'initial';
-    }
+	// show / hide facility line
+	if (goalFacility != homeFacility) currentFacilityLine = facilityLines[goalFacility.id];
+	if (currentFacilityLine) currentFacilityLine.color = (args.state === 'idle') ? inactiveLineCol : activeLineCol;
 
-    // show / hide facility line
-    if (goalFacility != homeFacility) currentFacilityLine = facilityLines[goalFacility.id];
-    if (currentFacilityLine) currentFacilityLine.color = (args.state === 'idle') ? inactiveLineCol : activeLineCol;
+	// request button
+	if (ownFacility != goalFacility && ownFacility != homeFacility) droneButtons.request.show(droneRequested);
+	else droneButtons.request.hide();
 
-    // show interactions
-    droneButtons.innerHTML = '';
-    if (canControl) {
-        switch (args.state) {
-            case 'awaiting_takeoff':
-                if (goalFacility != ownFacility) showTakeoff();
-                break;
-            case 'en_route':
-                showReturn();
-            case 'returning':
-                showLand();
-                break;
-            case 'emergency':
-                showEmergency();
-                break;
-        }
-    }
+	// allowTakeoff button
+	if (args.state == 'awaiting_takeoff' && goalFacility != ownFacility) droneButtons.allowTakeoff.show();
+	else droneButtons.allowTakeoff.hide();
+
+	// emergencyReturn button
+	if (args.state == 'en_route') droneButtons.emergencyReturn.show();
+	else droneButtons.emergencyReturn.hide();
+
+	// emergencyLand button
+	if (args.state == 'en_route' || args.state == 'returning') droneButtons.emergencyLand.show();
+	else droneButtons.emergencyLand.hide();
+
+	// crashed button
+	if (args.state == 'emergency') droneButtons.crashed.show();
+	else droneButtons.crashed.hide();
 }
 
 function onHeartbeat(args) {
-    console.log('onHeartbeat', args);
-    batteryDisplay.children[0].style.width = (args.battery*100)+'%';
-    droneMarker.setLatLng(args.pos);
+	console.log('onHeartbeat', args);
+	batteryDisplay.children[0].style.width = (args.battery*100)+'%';
+	droneMarker.setLatLng(args.pos);
 }
 
 function onDroneState(args) {
-    console.log('onDroneState', args);
-    let states = {
-        'idle': "Am Boden",
-        'en_route': "Fliegt",
-        'landing': "Landet",
-        'return_landing': "Landet",
-        'returning': "Kehrt zurück",
-        'emergency_landing': "Macht Notlandung",
-        'crashed': "Notgelandet",
-        'updating': "Empfängt Mission"
-    };
-    stateDisplay.innerText = states[args.state];
+	console.log('onDroneState', args);
+	let states = {
+		'idle': "Am Boden",
+		'en_route': "Fliegt",
+		'landing': "Landet",
+		'return_landing': "Landet",
+		'returning': "Kehrt zurück",
+		'emergency_landing': "Macht Notlandung",
+		'crashed': "Notgelandet",
+		'updating': "Empfängt Mission"
+	};
+	stateDisplay.innerText = states[args.state];
 }
 
 function onDroneRequested(args) {
-    console.log('onDroneRequested', args);
-    droneRequested = args.requested; // only true when we are waiting for the mission to start
-    showRequest();
+	console.log('onDroneRequested', args);
+	droneRequested = args.requested; // only true when we are waiting for the mission to start
+	if (ownFacility != goalFacility && ownFacility != homeFacility) droneButtons.request.show(droneRequested);
 }
